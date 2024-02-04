86eab273128981da257cf2ed7d3335966f03c00b
Irene Buceta <irenebuzalocen@gmail.com>
2024-02-04 19:47:58 +0100

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import './assets/css/fonts.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
